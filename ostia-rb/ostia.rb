a9a395e3610b4dae49f3a81f8a912692d2aecd4e
Andrea Nodari <andrea.nodari91@gmail.com>
2015-12-01 18:09:01 +0000

    - adds json format - change in CLI, not you have to specify the format when calling the script

--- ostia-rb/ostia.rb
+++ ostia-rb/ostia.rb
@@ -1,38 +1,46 @@
 #!/usr/bin/env ruby
 
 require 'ap'
+require 'json'
 
 #:nodoc:
 class DirectGraph
   def initialize
-    @nodes = {}
+    @nodes      = {}
+    @spouts_map = {}
+    @inverse    = {}
   end
 
   def add_link(from, to, label)
-    @nodes[from] ||= []
+    add_node(from)
+    add_node(to)
+
     @nodes[from] << { node: to, label: label }
+    @inverse[to] << { node: from, label: label }
     self
   end
 
-  def add_node(node)
-    @nodes[node] = []
+  def add_node(node, is_spout: false)
+    @spouts_map[node] ||= is_spout
+    @inverse[node] ||= []
+    @nodes[node] ||= []
     self
   end
 
-  def output(type)
-    case type
-    when :dot
-      generate_dot
-    else
-      puts "Available output format: .dot"
-      exit 1
-    end
+  def spouts
+    @spouts_map.select { |_, is_spout| is_spout }.keys
   end
 
-  private
+  def bolts
+    @nodes.keys - spouts
+  end
+
+  def in_edges(node)
+    @inverse[node].map { |h| h[:node] }
+  end
 
   def generate_dot
-    str  = "digraph G {"
+    str  = 'digraph G {'
     @nodes.each do |node, neighbors|
       next if neighbors.empty?
       neighbors.each do |destination|
@@ -44,16 +52,37 @@ class DirectGraph
     end
     str += "\n}"
   end
+
+  def generate_json
+    hash = {}
+    hash[:topology] = {}
+    hash[:topology][:spouts] = []
+    hash[:topology][:bolts]  = []
+
+    spouts.each do |spout|
+      hash[:topology][:spouts] << { id: spout }
+    end
+
+    bolts.each do |bolt|
+      hash[:topology][:bolts] << { id: bolt, subs: in_edges(bolt) }
+    end
+
+    hash.to_json
+  end
 end
 
 #:nodoc:
 class Ostia
+  OUTPUT_FORMATS = %w(dot json)
+
   def initialize(args)
-    if args.size != 1
-      puts 'Usage: ./ostia.rb <file_with_topologyBuilder>'
+    if args.size != 2 || !OUTPUT_FORMATS.include?(args.last)
+      puts "Usage: ./ostia.rb <file_with_topologyBuilder>\
+ <format: #{OUTPUT_FORMATS.join(', ')}>"
       exit 1
     end
     @filename = args.first
+    @format = args.last
     @graph = DirectGraph.new
   end
 
@@ -61,31 +90,31 @@ class Ostia
     @source_code = File.open(@filename).read
     lines = useful_lines
     generate_graph(lines)
-    puts @graph.output(:dot)
+    @graph.send("generate_#{@format}")
   end
 
   private
 
   def useful_lines
     # TODO: comments
-    spouts = @source_code.scan(%r{setSpout\("\w+".*\)})
-    bolts = @source_code.scan(%r{setBolt\("\w+".*\)(?:\s*\..*)*})
+    spouts = @source_code.scan(/setSpout\("\w+".*\)/)
+    bolts = @source_code.scan(/setBolt\("\w+".*\)(?:\s*\..*)*/)
             .map { |line| line.gsub(/\s/, '') }
     spouts + bolts
   end
 
   def generate_graph(lines)
-    # TODO: stream_id?
+    # TODO: stream_id
     lines.each do |line|
       methods = line.split('.')
       destination_node = extract_component_name(methods.first)
       if methods.size == 1
-        @graph.add_node(destination_node)
+        @graph.add_node(destination_node, is_spout: true)
       else
-        methods.each_with_index do |line, i|
+        methods.each_with_index do |method, i|
           next if i == 0
-          source_node = extract_component_name(line)
-          label = extract_label(line)
+          source_node = extract_component_name(method)
+          label = extract_label(method)
           @graph.add_link(source_node, destination_node, label)
         end
       end
@@ -101,4 +130,4 @@ class Ostia
   end
 end
 
-Ostia.new(ARGV).run
+puts Ostia.new(ARGV).run
